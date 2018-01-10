5d40b6113697929860b0d527e7456f0ef69f1d87
SamPratik <pratik.anwar@gmail.com>
2018-01-11 01:44:30 +0600

    initial commit

---
+++ src/pages/item-detail/item-detail.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ItemDetailPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-item-detail',
+  templateUrl: 'item-detail.html',
+})
+export class ItemDetailPage {
+  public title;
+  public description;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.title = this.navParams.get('item').title;
+    this.description = this.navParams.get('item').description;
+  }
+
+}
